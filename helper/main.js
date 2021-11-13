4804ea78eef69882c53062db9d6683b6d77a7b25
SamuelFoc <samo.sipikal@gmail.com>
2021-11-13 23:27:08 +0100

    horizontal scrolling

--- helper/main.js
+++ helper/main.js
@@ -1,13 +1,44 @@
-const box1 = document.getElementById("body");
-const halfWidth = window.innerWidth/2;
-const halfHeight = window.innerHeight/2;
+(function(){
+    init();
 
-box1.addEventListener("mousemove", (e) => {
-    let offsetY = halfHeight - e.clientY;
-    let offsetX = halfWidth - e.clientX;
-    let distance = Math.sqrt(Math.pow(offsetX, 2) + Math.pow(offsetY, 2));
-    box1.style.textShadow = offsetX + "px " + offsetY + "px " + distance/20 + "px black";
-    console.log(offsetY);
-});
+    var g_containerInViewport;
+    function init(){
+        setStickyContainersSize();
+        bindEvents();
+    }
 
+    function bindEvents(){
+        window.addEventListener("wheel", wheelHandler);        
+    }
 
+    function setStickyContainersSize(){
+        document.querySelectorAll('.sticky-container').forEach(function(container){
+            const stikyContainerHeight = (container.querySelector('main').offsetWidth + window.innerHeight);
+            container.setAttribute('style', 'height: ' + stikyContainerHeight + 'px');
+        });
+    }
+
+    function isElementInViewport (el) {
+        const rect = el.getBoundingClientRect();
+        return rect.top <= 0 && rect.bottom > document.documentElement.clientHeight;
+    }
+
+    function wheelHandler(evt){
+        
+        const containerInViewPort = Array.from(document.querySelectorAll('.sticky-container')).filter(function(container){
+            return isElementInViewport(container);
+        })[0];
+
+        if(!containerInViewPort){
+            return;
+        }
+
+        var isPlaceHolderBelowTop = containerInViewPort.offsetTop < document.documentElement.scrollTop;
+        var isPlaceHolderBelowBottom = containerInViewPort.offsetTop + containerInViewPort.offsetHeight > document.documentElement.scrollTop;
+        let g_canScrollHorizontally = isPlaceHolderBelowTop && isPlaceHolderBelowBottom;
+
+        if(g_canScrollHorizontally){
+            containerInViewPort.querySelector('main').scrollLeft += evt.deltaY;
+        }
+    }
+})();
